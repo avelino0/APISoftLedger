deb7a15b02515b45532936503c2fe90298b8e324
Amanda Avelino <amanda.avelino@rochadigital.com>
2026-03-06 10:49:51 -0300

    fix

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run any pre-start commands, e.g., database migrations
+# dotnet ef database update # (Example migration command, requires EF tools)
+
+# Start the application
+dotnet run
