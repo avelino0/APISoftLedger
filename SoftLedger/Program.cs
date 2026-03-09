2a8f39a707fc3c32e78eaaff99161d48ccd55fc7
Saviotti, Otavio V (Contractor) <otaviovsaviotti@gmail.com>
2026-03-09 15:29:20 -0300

    program.cs

--- SoftLedger/Program.cs
+++ SoftLedger/Program.cs
@@ -9,26 +9,26 @@ builder.Services.AddControllers();
 var key = builder.Configuration["JwtKey"];
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)
-        )
-    };
-});
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(key))
+        };
+    });
 
 var app = builder.Build();
-app.Run($"http://0.0.0.0:{port}");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();//
+app.Run($"http://0.0.0.0:{port}");
