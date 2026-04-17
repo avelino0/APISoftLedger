dabf7a9bf84c5965858f68e60f6b5259dfae9ebc
Amanda Avelino <amanda.avelino@rochadigital.com>
2026-04-17 11:30:29 -0300

    [feat] Autenticação

--- SoftLedger/Controllers/AuthController.cs
+++ SoftLedger/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,31 +13,60 @@ namespace SoftLedger.Controllers
     {
         private readonly IConfiguration _configuration;
 
+
+        private readonly List<(string Email, string Password)> _users = new()
+
+        {
+
+            ("admin@teste.com", "123456")
+
+        };
+
+
         public AuthController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
+
+
         [HttpPost("login")]
-        public IActionResult Login()
+        public IActionResult Login([FromBody] LoginRequest request)
+
         {
+            var user = _users.FirstOrDefault(u =>
+            u.Email == request.Email &&
+            u.Password == request.Password
+
+        );
+
+            if (user == default)
+
+            return Unauthorized("Email ou senha inválidos");
+
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
 
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var claims = new[]
+
             {
-                new Claim(ClaimTypes.Name, "usuario")
+                new Claim(ClaimTypes.Name, request.Email)
             };
 
             var token = new JwtSecurityToken(
+
                 claims: claims,
+
                 expires: DateTime.Now.AddHours(1),
-                signingCredentials: creds
-            );
+
+                signingCredentials: creds);
+
 
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
+
             return Ok(new { token = jwt });
+
         }
     }
 }
